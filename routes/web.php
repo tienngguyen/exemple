a1ae5bfbea9ac27fd121ef79c364fc8776803146
tiennv <tien.nguyen.van@fujitechjsc.com>
2021-07-12 16:04:15 +0700

    update source frontend

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,8 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('home.content');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
