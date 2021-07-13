5d58708780401b79a8cc90064445358c22e7ca28
tiennv <tien.nguyen.van@fujitechjsc.com>
2021-07-13 11:12:24 +0700

    update code

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -1,9 +1,9 @@
 <div class="footer clearfix mb-0 text-muted">
     <div class="float-start">
-        <p>2021 &copy; Mazer</p>
+        <p>2021 &copy; Tien</p>
     </div>
     <div class="float-end">
         <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                href="http://ahmadsaugi.com">A. Saugi</a></p>
+                href="http://ahmadsaugi.com">Tien Ng</a></p>
     </div>
 </div>
